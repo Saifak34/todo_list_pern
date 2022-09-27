e40f7963198fce16e8dc709f316f968f7c7a654f
Saifalikhan Pathan <78328030+Saifak34@users.noreply.github.com>
2022-09-26 21:58:34 -0400

    changing field

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user:"postgres",
-    password:"Zs*1623116231",
+    password:"*****",
     host: "localhost",
     port: 5432,
     database:"perntodo"
